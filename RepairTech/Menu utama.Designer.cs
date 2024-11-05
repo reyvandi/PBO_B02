ee7028651295ce148a9f4a52a6783095bdf98474
Adityapanji03 <149854179+Adityapanji03@users.noreply.github.com>
2024-11-05 01:26:21 -0800

    Add files via upload
    menu utm nyar

--- RepairTech/Menu utama.Designer.cs
+++ RepairTech/Menu utama.Designer.cs
@@ -28,115 +28,306 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu_utama));
             panel1 = new Panel();
-            label1 = new Label();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
+            panel3 = new Panel();
             button5 = new Button();
             button6 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel2 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            button7 = new Button();
+            panel4 = new Panel();
+            panel5 = new Panel();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(12, 31);
-            button1.Name = "button1";
-            button1.Size = new Size(236, 76);
-            button1.TabIndex = 0;
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.Magenta;
-            panel1.Controls.Add(button6);
+            panel1.BackColor = Color.FromArgb(24, 30, 54);
+            panel1.Controls.Add(panel3);
             panel1.Controls.Add(button5);
+            panel1.Controls.Add(button6);
             panel1.Controls.Add(button4);
             panel1.Controls.Add(button3);
             panel1.Controls.Add(button2);
-            panel1.Controls.Add(label1);
             panel1.Controls.Add(button1);
-            panel1.Location = new Point(0, 1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(264, 703);
+            panel1.Size = new Size(185, 448);
             panel1.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Cyan;
+            panel3.Location = new Point(0, 143);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(7, 40);
+            panel3.TabIndex = 14;
+            //panel3.Paint += this.panel3_Paint;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(24, 30, 54);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(0, 403);
+            button5.Name = "button5";
+            button5.Size = new Size(185, 42);
+            button5.TabIndex = 11;
+            button5.Text = "Logout";
+            button5.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(24, 30, 54);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Image = Properties.Resources.home_9187548;
+            button6.Location = new Point(-3, 332);
+            button6.Name = "button6";
+            button6.Size = new Size(185, 42);
+            button6.TabIndex = 11;
+            button6.Text = "Teknisi";
+            button6.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(24, 30, 54);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Image = Properties.Resources.home_9187548;
+            button4.Location = new Point(-3, 287);
+            button4.Name = "button4";
+            button4.Size = new Size(185, 42);
+            button4.TabIndex = 11;
+            button4.Text = "Jasa Servis";
+            button4.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(24, 30, 54);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Image = Properties.Resources.home_9187548;
+            button3.Location = new Point(-3, 239);
+            button3.Name = "button3";
+            button3.Size = new Size(185, 42);
+            button3.TabIndex = 11;
+            button3.Text = "Komponen";
+            button3.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(24, 30, 54);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(-3, 191);
+            button2.Name = "button2";
+            button2.Size = new Size(185, 42);
+            button2.TabIndex = 11;
+            button2.Text = "Transaksi";
+            button2.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(24, 30, 54);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Nirmala UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(-3, 143);
+            button1.Name = "button1";
+            button1.Size = new Size(185, 42);
+            button1.TabIndex = 11;
+            button1.Text = "Dashboard";
+            button1.TextImageRelation = TextImageRelation.TextBeforeImage;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(179, 144);
+            panel2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Nirmala UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(66, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 17);
+            label2.TabIndex = 10;
+            label2.Text = "Admin";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 163);
+            label1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(0, 146, 159);
+            label1.Location = new Point(47, 84);
             label1.Name = "label1";
-            label1.Size = new Size(56, 30);
-            label1.TabIndex = 1;
-            label1.Text = "Fitur";
-            label1.Click += label1_Click;
+            label1.Size = new Size(85, 18);
+            label1.TabIndex = 10;
+            label1.Text = "Username";
             // 
-            // button2
+            // pictureBox1
             // 
-            button2.Location = new Point(12, 228);
-            button2.Name = "button2";
-            button2.Size = new Size(237, 39);
-            button2.TabIndex = 8;
-            button2.UseVisualStyleBackColor = true;
+            pictureBox1.Image = Properties.Resources.user;
+            pictureBox1.Location = new Point(57, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(63, 63);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
-            // button3
+            // label3
             // 
-            button3.Location = new Point(12, 273);
-            button3.Name = "button3";
-            button3.Size = new Size(237, 39);
-            button3.TabIndex = 9;
-            button3.UseVisualStyleBackColor = true;
+            label3.AutoSize = true;
+            label3.Location = new Point(0, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 10;
+            label3.Text = "label3";
             // 
-            // button4
+            // label4
             // 
-            button4.Location = new Point(12, 318);
-            button4.Name = "button4";
-            button4.Size = new Size(237, 39);
-            button4.TabIndex = 9;
-            button4.UseVisualStyleBackColor = true;
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(46, 51, 73);
+            label4.Font = new Font("Nirmala UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(158, 161, 176);
+            label4.Location = new Point(188, 13);
+            label4.Name = "label4";
+            label4.Size = new Size(138, 32);
+            label4.TabIndex = 11;
+            label4.Text = "Dashboard";
+            label4.Click += label4_Click;
             // 
-            // button5
+            // textBox1
             // 
-            button5.Location = new Point(11, 363);
-            button5.Name = "button5";
-            button5.Size = new Size(237, 39);
-            button5.TabIndex = 9;
-            button5.UseVisualStyleBackColor = true;
+            textBox1.BackColor = Color.FromArgb(74, 79, 99);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Arial Narrow", 9.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(431, 22);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(245, 23);
+            textBox1.TabIndex = 12;
+            textBox1.Text = "Search . . .";
             // 
-            // button6
+            // button7
             // 
-            button6.Location = new Point(11, 651);
-            button6.Name = "button6";
-            button6.Size = new Size(237, 39);
-            button6.TabIndex = 9;
-            button6.UseVisualStyleBackColor = true;
+            button7.BackColor = Color.FromArgb(24, 30, 54);
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.ForeColor = SystemColors.ButtonHighlight;
+            button7.Location = new Point(682, 22);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 13;
+            button7.Text = "Search";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Gray;
+            panel4.Location = new Point(190, 105);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(567, 79);
+            panel4.TabIndex = 14;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Gray;
+            panel5.Location = new Point(190, 191);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(567, 79);
+            panel5.TabIndex = 15;
             // 
             // Menu_utama
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.MenuHighlight;
-            ClientSize = new Size(1099, 747);
+            BackColor = Color.FromArgb(46, 51, 73);
+            ClientSize = new Size(769, 448);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(button7);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(panel1);
+            Margin = new Padding(2);
             Name = "Menu_utama";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Menu_utama";
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
-        private Button button4;
-        private Button button6;
         private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
         private Label label1;
+        private Button button1;
+        private Label label2;
         private Button button5;
+        private Button button6;
+        private Button button4;
         private Button button3;
+        private Button button2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private Button button7;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
     }
 }
--- RepairTech/Menu utama.cs
+++ RepairTech/Menu utama.cs
@@ -31,5 +31,35 @@ namespace PROJECT_PBO
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
