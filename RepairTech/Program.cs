d9851b82bfbc73235b71f498a485a9025d5daf90
Adityapanji03 <Adityapanjisaja@gmail.com>
2024-11-30 16:37:31 -0800

    desain addTeknisi

--- RepairTech/Program.cs
+++ RepairTech/Program.cs
@@ -13,7 +13,7 @@ namespace PROJECT_PBO
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AddJasaPerbaikanForm());
+            Application.Run(new LOGIN());
         }
     }
 }
--- RepairTech/View/AddTeknisiForm.Designer.cs
+++ RepairTech/View/AddTeknisiForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddTeknisiForm));
             label1 = new Label();
             textBox1 = new TextBox();
             label2 = new Label();
@@ -40,7 +41,6 @@
             textBox3 = new TextBox();
             buttonCancel = new Button();
             buttonAdd = new Button();
-            label6 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -48,20 +48,19 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(820, 260);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(858, 113);
             label1.Name = "label1";
-            label1.Size = new Size(74, 30);
+            label1.Size = new Size(51, 20);
             label1.TabIndex = 0;
             label1.Text = "Nama";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(820, 294);
-            textBox1.Margin = new Padding(4);
+            textBox1.Location = new Point(858, 145);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(331, 34);
+            textBox1.Size = new Size(233, 22);
             textBox1.TabIndex = 1;
             // 
             // label2
@@ -69,10 +68,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.Control;
-            label2.Location = new Point(819, 358);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(858, 202);
             label2.Name = "label2";
-            label2.Size = new Size(101, 30);
+            label2.Size = new Size(69, 20);
             label2.TabIndex = 2;
             label2.Text = "Keahlian";
             label2.Click += label2_Click;
@@ -81,10 +79,10 @@
             // 
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Software", "Hardware" });
-            comboBox1.Location = new Point(820, 392);
-            comboBox1.Margin = new Padding(4);
+            comboBox1.Location = new Point(858, 237);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(335, 33);
+            comboBox1.Size = new Size(236, 23);
             comboBox1.TabIndex = 3;
             // 
             // label3
@@ -92,10 +90,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.Control;
-            label3.Location = new Point(820, 452);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(858, 291);
             label3.Name = "label3";
-            label3.Size = new Size(112, 30);
+            label3.Size = new Size(76, 20);
             label3.TabIndex = 4;
             label3.Text = "Jam kerja";
             label3.Click += label3_Click;
@@ -104,10 +101,10 @@
             // 
             comboBox2.FormattingEnabled = true;
             comboBox2.Items.AddRange(new object[] { "07.00 - 12.00", "12.00 -17.00" });
-            comboBox2.Location = new Point(819, 486);
-            comboBox2.Margin = new Padding(4);
+            comboBox2.Location = new Point(858, 325);
+            comboBox2.Margin = new Padding(3, 2, 3, 2);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(336, 33);
+            comboBox2.Size = new Size(236, 23);
             comboBox2.TabIndex = 5;
             // 
             // label4
@@ -115,20 +112,19 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = SystemColors.Control;
-            label4.Location = new Point(820, 547);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(858, 379);
             label4.Name = "label4";
-            label4.Size = new Size(87, 30);
+            label4.Size = new Size(60, 20);
             label4.TabIndex = 6;
             label4.Text = "Alamat";
             label4.Click += label4_Click;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(820, 590);
-            textBox2.Margin = new Padding(4);
+            textBox2.Location = new Point(858, 416);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(335, 31);
+            textBox2.Size = new Size(236, 23);
             textBox2.TabIndex = 7;
             // 
             // label5
@@ -136,20 +132,19 @@
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.ForeColor = SystemColors.Control;
-            label5.Location = new Point(819, 654);
-            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Location = new Point(858, 473);
             label5.Name = "label5";
-            label5.Size = new Size(130, 30);
+            label5.Size = new Size(89, 20);
             label5.TabIndex = 8;
             label5.Text = "No Telepon";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(820, 693);
-            textBox3.Margin = new Padding(4);
+            textBox3.Location = new Point(858, 508);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(335, 38);
+            textBox3.Size = new Size(236, 24);
             textBox3.TabIndex = 9;
             // 
             // buttonCancel
@@ -157,10 +152,10 @@
             buttonCancel.BackColor = Color.Red;
             buttonCancel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             buttonCancel.ForeColor = SystemColors.Control;
-            buttonCancel.Location = new Point(13, 1091);
-            buttonCancel.Margin = new Padding(4);
+            buttonCancel.Location = new Point(9, 655);
+            buttonCancel.Margin = new Padding(3, 2, 3, 2);
             buttonCancel.Name = "buttonCancel";
-            buttonCancel.Size = new Size(182, 54);
+            buttonCancel.Size = new Size(127, 32);
             buttonCancel.TabIndex = 10;
             buttonCancel.Text = "Cancel";
             buttonCancel.UseVisualStyleBackColor = false;
@@ -171,34 +166,23 @@
             buttonAdd.BackColor = Color.DarkBlue;
             buttonAdd.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
             buttonAdd.ForeColor = SystemColors.Control;
-            buttonAdd.Location = new Point(845, 789);
-            buttonAdd.Margin = new Padding(4);
+            buttonAdd.Location = new Point(927, 568);
+            buttonAdd.Margin = new Padding(3, 2, 3, 2);
             buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new Size(273, 53);
+            buttonAdd.Size = new Size(191, 32);
             buttonAdd.TabIndex = 11;
             buttonAdd.Text = "Submit";
             buttonAdd.UseVisualStyleBackColor = false;
             buttonAdd.Click += buttonAdd_Click;
             // 
-            // label6
-            // 
-            label6.BackColor = Color.DarkBlue;
-            label6.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.ForeColor = SystemColors.Control;
-            label6.Location = new Point(0, -1);
-            label6.Name = "label6";
-            label6.Size = new Size(1929, 108);
-            label6.TabIndex = 12;
-            label6.Text = "Tambah Teknisi";
-            label6.TextAlign = ContentAlignment.MiddleCenter;
-            // 
             // AddTeknisiForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(24, 23, 80);
-            ClientSize = new Size(1924, 1170);
-            Controls.Add(label6);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1347, 637);
             Controls.Add(buttonAdd);
             Controls.Add(buttonCancel);
             Controls.Add(textBox3);
@@ -211,8 +195,9 @@
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
+            DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
-            Margin = new Padding(4);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "AddTeknisiForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "AddTeknisiForm";
@@ -236,6 +221,5 @@
         private TextBox textBox3;
         private Button buttonCancel;
         private Button buttonAdd;
-        private Label label6;
     }
 }
